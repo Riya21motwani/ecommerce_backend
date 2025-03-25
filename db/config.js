5df2bd3c6b7387cefa8ece96b874bcc9e8b40045
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2025-03-25 12:14:23 +0530

    initial commit

---
+++ db/config.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+// Connecting to MongoDB database
+mongoose.connect("mongodb://mongodb+srv://aayush2705gupta:1XCLiohbyRiwPwGm@cluster0.fqqki.mongodb.net/e-commerce", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to MongoDB database"); // Log to confirm connection
+}).catch((error) => {
+    console.error("Error connecting to MongoDB:", error); // Log error if connection fails
+});
